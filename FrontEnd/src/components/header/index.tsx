f0f02484c75851133b2b1980c80c61e327debe74
AC-Oliveira <allancardoso21@gmail.com>
2022-11-16 21:40:59 -0300

    Change header logo

--- FrontEnd/src/components/header/index.tsx
+++ FrontEnd/src/components/header/index.tsx
@@ -8,8 +8,8 @@ export function Header(): JSX.Element {
 
   return (
     <nav className="min-vw-100 container container-sm navbar navbar-expand-lg navbar-light bg-white d-flex align-items-center border-bottom justify-content-between">
-      <div className="d-flex align-items-center gap-3 mx-auto mb-2 mb-sm-0">
-        <div className="logo d-flex justify-content-ceter align-items-center ms-3 bg-gray-100 rounded-circle">
+      <div className="d-flex align-items-center gap-2 mx-auto mb-2 mb-sm-0">
+        <div className="logo d-flex justify-content-ceter align-items-center">
           <img src="logo.png" alt="Inga code logo" />
         </div>
         <h4 className="mb-0 NavTitle">TaskTracker</h4>
